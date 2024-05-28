3f3724299466b46052bc8c1de5350f8a605b5eb9
Kovalev_Andrey <andy.kovalev2002@gmail.com>
2024-05-28 18:24:40 +0300

    Setup added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convert',
+    version='0.1',
+    description='Python library for conversion skeleton models to universal model',
+    author='Kovalev Andrey',
+    packages=find_packages(),
+)
